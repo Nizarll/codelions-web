c09c76ee7f600e652b2b83bae8e1d4701ff308ab
Nizarll <forsubsofmy@gmail.com>
2024-08-12 22:08:08 +0000

    last update

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -9,7 +9,8 @@ export default function Form() {
         Got a technical issue? Want to send feedback about a beta feature? Need details about our Business plan? Let us know.
       </p>
         <form action="#" className="space-y-8">
-          <div>
+        <div class="flex flex-row">
+          <div class="mr-8 w-[60%]">
             <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">
             Your email
           </label>
@@ -18,8 +19,21 @@ export default function Form() {
             id="email"
             className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
             placeholder="name@gmail.com"
-            required
-          />
+            required/>
+          </div>
+
+          <div class="w-[40%]">
+            <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+            Your phone number 
+          </label>
+            <input
+            type="email"
+            id="number"
+            className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+            placeholder="+212"
+            required/>
+          </div>
+
           </div>
           <div>
             <label htmlFor="subject" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">
